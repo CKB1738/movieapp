3c511022be0fbaf1ea5debfa060e673a81584587
Arshdeep Singh Sahota <sahotaa1@myumanitoba.ca>
2023-03-10 11:50:42 -0600

    Update Service.java

--- app/src/main/java/comp3350/go4tv/application/Service.java
+++ app/src/main/java/comp3350/go4tv/application/Service.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import comp3350.go4tv.objects.Movie;
 import comp3350.go4tv.persistence.MoviePersistence;
 import comp3350.go4tv.persistence.UserPersistence;
+import comp3350.go4tv.persistence.hsqldb.MoviePersistenceHSQLDB;
 import comp3350.go4tv.persistence.hsqldb.UserPersistenceHSQLDB;
 import comp3350.go4tv.persistence.stubs.MoviePersistenceStub;
 import comp3350.go4tv.persistence.stubs.UserPersistenceStub;
@@ -24,12 +25,10 @@ public class Service {
 
     public static synchronized MoviePersistence getMoviePersistence(){
         if(moviePersistence == null){
-            moviePersistence = new MoviePersistenceStub();
-
+            moviePersistence = new MoviePersistenceHSQLDB(Main.getDBPathName());
         }
 
         return moviePersistence;
     }
-
 
 }
